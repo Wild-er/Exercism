aaf58f612ce6e9617ddf30f8bc7d38d78eda146f
Wild-er <wilder@algogems.io>
2025-05-31 10:27:02 -0400

    First attempt. Trying out engineering format for large numbers.

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * Date passed to us will be in milliseconds.
+ * 
+ * We need to find the day that is 1,000,000,000 ahead of that.
+ * 
+ * 1e+9 seconds = 1e+12 miliseconds
+ * 
+ * 
+ * 
+ * @param {*} date 
+ */
+
+export const gigasecond = (date) => {
+  return date + 1e+12; // Return the date a gigasecond later
+};
