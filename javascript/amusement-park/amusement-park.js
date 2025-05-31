60923ef097da38d149ecf7e23116dacf1159a574
Wild-er <wilder@algogems.io>
2025-05-31 13:53:36 -0400

    Try check for undefined first

--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -37,11 +37,11 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-    if (tickets[ticketId] === null) {
-    return 'not sold';
+    if (tickets.ticketId === undefined) {
+    return 'unkown ticket ID';
   }
-  else if (tickets[ticketId] === undefined) {
-    return 'unknown ticket id';
+  else if (tickets.ticketId === null) {
+    return 'not sold';
   }
   return 'sold to ' + tickets[ticketId];
 }
