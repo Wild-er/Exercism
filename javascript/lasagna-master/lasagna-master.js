8f369ba8dc536181e13872635f4a2b2f3001648c
Wild-er <wilder@algogems.io>
2025-05-31 22:46:53 -0400

    typo

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -19,9 +19,55 @@ else if (minutes > 0) {
 return 'You forgot to set the timer.'
  }
 
-export function preperationTime(layers, time) {
-    if (time) {
-        return layers.length * time;
+/** 
+ * Implement a function that calculates the prep
+ * time of a lasagna based on layers and time to 
+ * prep them.
+ * 
+ * @param {array} layers Layers by name
+ * @param {number} time Time to prep each layer
+ * @returns {number} 
+ */
+export function preparationTime(layers, time) {
+    if (time) { // If time exists, TRUTHY
+        return layers.length * time; 
     }
-    return layers.length * 2;
+    return layers.length * 2; // if no time param, use the default of 2.
+}
+
+/**
+ * Calculate the ingredient amounts of noodles and sauce in a lasagana layers list or recipe.
+ * 
+ * @param {array} layers 
+ * @returns 
+ */
+export function quantities(layers) {
+    let n = 0; // noodles count in g
+    let s = 0; // sauce count in L
+    for (let a in layers) {
+        switch (a) {
+            case 'noodles':
+                n += 50;
+                break;
+            case 'sauce':
+                s += 0.2;
+                break;
+        }
+    }
+    let o = {
+        noodles: n,
+        sauce: s
+    }
+    return o;
+}
+
+export function addSecretIngredient(array1, array2) {
+    array1.push(array2.pop());
+}
+
+export function scaleRecipe(recipe, portions) {
+    for (let i in recipe) {
+        recipe[i] *= portions;
+    }
+    return recipe;
 }
