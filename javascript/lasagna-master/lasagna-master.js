aac7d6d4ec518098cacfdcd6bf61b91728a5b7d7
Wild-er <wilder@algogems.io>
2025-05-31 21:42:50 -0400

    First attempt

---
+++ javascript/lasagna-master/lasagna-master.js
@@ -0,0 +1,21 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Implement the functions needed to solve the exercise here.
+ * Do not forget to export them so they are available for the
+ * tests. Here an example of the syntax as reminder:
+ * @param minutes Amount of time left to cook
+ * @returns {string} Cooking message
+ *
+ */
+export function lasagna(minutes) {
+if (minutes === 0) {
+    return 'Lasagna is done.'
+}
+else if (minutes > 0) {
+    return 'Not done, please wait.'
+}
+return 'You forgot to set the timer.'
+ }
+
